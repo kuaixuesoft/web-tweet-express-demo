510163e96a71315567f88b40d9a5258ecaa8cc88
James Ji <github@chinagood.net>
2019-02-07 20:32:47 -0800

    finish lesson-2

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-const express = require("express")
-
+const express = require('express');
 const app = express();
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-app.get('/', (req,res) => res.send("hello world")).listen();
+/**
+ * middleware
+ * note: middleware is running in sequence, from top to bottom
+ */
+app.use(logger('dev')); // log requests in server console
+app.use(bodyParser.json()); // parse client request data to json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, ()=> console.log('listening on port 3000'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err =  new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next)=> {
+  //res.status(404);
+  res.send(err.message);
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || 3000;
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+app.listen(port, ()=>{
+  console.log(`Server running on http://localhost:${port}`)
+});
